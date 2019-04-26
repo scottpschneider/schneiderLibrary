91d37906ce525903651d456eadc798e9b019a262
Scott Schneider <scott.schneider@intimetec.com>
2019-04-26 12:32:09 -0600

    Import NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 
@@ -10,7 +10,8 @@ import { BooksComponent } from './books/books.component';
     BooksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
